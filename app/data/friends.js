49e830b435a99966dfed886586f5d3a78a66b8c6
Richard Taing <Richard.Taing@pexa.com.au>
2019-09-14 21:20:24 +1000

    Added my friends

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friends = [
+  {
+    name: "Chad 'The Model'",
+    photo: "images/Chad.jpg",
+    scores: ["2", "1", "1", "5", "5", "1", "3", "1", "1", "5"]
+  },
+  {
+    name: "Jose Alipha",
+    photo: "images/brazilianMan.jpg",
+    scores: ["1", "1", "5", "5", "5", "1", "1", "5", "5", "1"]
+  },
+  {
+    name: "Alessandra",
+    photo: "images/alessandraAmbrosio.jpg",
+    scores: ["5", "2", "1", "1", "1", "1", "5", "1", "3", "3"]
+  },
+  {
+    name: "Lais",
+    photo: "images/laisRibeiro.jpg",
+    scores: ["5", "1", "5", "1", "3", "1", "1", "1", "5", "3"]
+  }
+];
+
+module.exports = friends;
