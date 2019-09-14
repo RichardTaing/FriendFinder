1e1d893a73508fe13e55134e03cc837e2cf13285
Richard Taing <Richard.Taing@pexa.com.au>
2019-09-14 20:58:13 +1000

    added routing for survey abd home

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,18 @@
+//Dependencies - include the path for out html
+
+var path = require("path");
+
+//Routing
+
+module.exports = function(app) {
+  //HTML get requests
+  //Below code handles when users "visits" a page
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
